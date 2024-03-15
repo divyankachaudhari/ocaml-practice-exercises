abf44eef0bb104ee17785a22540cc626dc6b5fa4
Divyanka Chaudhari <60309618+divyankachaudhari@users.noreply.github.com>
2024-03-15 11:43:46 +0530

    Added sync_repo.sh

--- sync_repo.sh
+++ sync_repo.sh
@@ -4,11 +4,4 @@
 
 git remote add upstream https://github.com/ocaml/ocaml.org.git
 git fetch upstream
-
-mkdir -p temp_practice
 git checkout upstream/main -- practice
-mv practice temp_practice/
-
-rsync -av --include '*/' --include '00[1-9]/' --include '0[1-9][0-9]/' --include '09[0-3]/' --exclude '*' temp_practice/practice/ .
-
-rm -rf temp_practice
