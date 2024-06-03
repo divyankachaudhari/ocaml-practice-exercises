e2d70bea7aea35711f33e2539fb12231e5e1092b
Divyanka Chaudhari <60309618+divyankachaudhari@users.noreply.github.com>
2024-06-03 17:04:25 +0530

    Update replit.nix

--- replit.nix
+++ replit.nix
@@ -7,4 +7,12 @@
     pkgs.ocamlPackages.curly
     pkgs.ocamlPackages.ounit2
   ];
+
+  shellHook = ''
+    export OPAMYES=1
+    opam init --disable-sandboxing --bare
+    opam switch create . ocaml-base-compiler.4.14.0
+    eval $(opam env)
+    opam install dune ounit2 ocaml-lsp-server ocamlformat curly
+  '';
 }
