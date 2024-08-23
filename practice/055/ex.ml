264eca963dae79cceb9af9d669e65c432776a139
Cuihtlauac ALVARADO <cuihtmlauac@tarides.com>
2024-08-23 16:05:02 +0200

    Fix ex 55

--- practice/055/ex.ml
+++ practice/055/ex.ml
@@ -10,14 +10,15 @@ end
 module Make(Tested: Testable) : sig val run : unit -> unit end = struct
   open Tested
 
-  let tree_pp pp fmt = function
+  let rec tree_pp pp fmt = function
     | Empty -> Format.fprintf fmt "Empty"
     | Node (v, l, r) -> Format.fprintf fmt "Node (%a, %a, %a)" pp v (tree_pp pp) l (tree_pp pp) r
 
   let trees_pp pp fmt trees =
     Format.fprintf fmt "[%a]" (Format.pp_print_list ~pp_sep:Format.pp_print_space (tree_pp pp)) trees
 
-  let tree_equal a b = 
+  (*
+  let rec tree_equal a b = 
     match a, b with
     | Empty, Empty -> true
     | Node (va, la, ra), Node (vb, lb, rb) -> va = vb && tree_equal la lb && tree_equal ra rb
@@ -25,6 +26,7 @@ module Make(Tested: Testable) : sig val run : unit -> unit end = struct
 
   let trees_equal a b =
     List.length a = List.length b && List.for_all2 tree_equal a b
+*)
 
   let test_example_test () =
     let result = cbal_tree 4 in
@@ -34,24 +36,24 @@ module Make(Tested: Testable) : sig val run : unit -> unit end = struct
       Node ('x', Node ('x', Node ('x', Empty, Empty), Empty), Node ('x', Empty, Empty));
       Node ('x', Node ('x', Empty, Node ('x', Empty, Empty)), Node ('x', Empty, Empty))
     ] in
-    check (testable trees_pp trees_equal) "Example Test" expected_result result
+    check (of_pp (trees_pp (fun out -> Format.fprintf out "%c"))) "Example Test" expected_result result
 
   let test_empty_tree () =
     let result = cbal_tree 0 in
     let expected_result = [Empty] in
-    check (testable trees_pp trees_equal) "Empty Tree Test" expected_result result
+    check (of_pp (trees_pp (fun out -> Format.fprintf out "%c"))) "Empty Tree Test" expected_result result
 
   let test_single_node () =
     let result = cbal_tree 1 in
     let expected_result = [Node ('x', Empty, Empty)] in
-    check (testable trees_pp trees_equal) "Single Node Test" expected_result result
+    check (of_pp (trees_pp (fun out -> Format.fprintf out "%c"))) "Single Node Test" expected_result result
 
   let test_multi_nodes () =
     let result = cbal_tree 3 in
     let expected_result = [
       Node ('x', Node ('x', Empty, Empty), Node ('x', Empty, Empty))
     ] in
-    check (testable trees_pp trees_equal) "Multi Nodes Test" expected_result result
+    check (of_pp (trees_pp (fun out -> Format.fprintf out "%c"))) "Multi Nodes Test" expected_result result
 
   let run () =
     let open Alcotest in
