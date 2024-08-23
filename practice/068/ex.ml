69df7ddffc4820f35e661ddc1ed99bcc05ad4bf1
Cuihtlauac ALVARADO <cuihtmlauac@tarides.com>
2024-08-23 17:38:43 +0200

    Fix 63-65,68

--- practice/068/ex.ml
+++ practice/068/ex.ml
@@ -12,16 +12,16 @@ end
 module Make(Tested: Testable) : sig val run : unit -> unit end = struct
   open Tested
 
-  let tree_pp pp fmt = function
+  let rec tree_pp pp fmt = function
     | Empty -> Format.fprintf fmt "Empty"
     | Node (v, l, r) -> Format.fprintf fmt "Node (%a, %a, %a)" pp v (tree_pp pp) l (tree_pp pp) r
-
+(*
   let tree_equal a b = 
     match a, b with
     | Empty, Empty -> true
     | Node (va, la, ra), Node (vb, lb, rb) -> va = vb && tree_equal la lb && tree_equal ra rb
     | _ -> false
-
+*)
   let test_preorder_works_correctly () =
     let example_tree =
       Node ('a', Node ('b', Node ('d', Empty, Empty), Node ('e', Empty, Empty)),
@@ -42,7 +42,7 @@ module Make(Tested: Testable) : sig val run : unit -> unit end = struct
     let expected_tree =
       Node ('a', Node ('b', Node ('d', Empty, Empty), Node ('e', Empty, Empty)),
            Node ('c', Empty, Node ('f', Node ('g', Empty, Empty), Empty))) in
-    check (testable (tree_pp Format.pp_print_char) tree_equal)
+    check (of_pp (tree_pp Format.pp_print_char))
       "pre_in_tree" expected_tree (pre_in_tree pre_seq in_seq)
 
   let run () =
