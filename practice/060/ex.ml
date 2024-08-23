5cc1555462f2603a0a082ef2cdaed7c944729bfe
Cuihtlauac ALVARADO <cuihtmlauac@tarides.com>
2024-08-23 16:50:28 +0200

    Fix 060

--- practice/060/ex.ml
+++ practice/060/ex.ml
@@ -12,21 +12,21 @@ end
 module Make(Tested: Testable) : sig val run : unit -> unit end = struct
   open Tested
 
-  let tree_pp pp fmt = function
+  let rec tree_pp pp fmt = function
     | Empty -> Format.fprintf fmt "Empty"
     | Node (v, l, r) -> Format.fprintf fmt "Node (%a, %a, %a)" pp v (tree_pp pp) l (tree_pp pp) r
-
+(*
   let tree_equal a b = 
     match a, b with
     | Empty, Empty -> true
     | Node (va, la, ra), Node (vb, lb, rb) -> va = vb && tree_equal la lb && tree_equal ra rb
     | _ -> false
-
+*)
   let test_hbal_tree_nodes () =
     let test_height_1 () =
       let trees = hbal_tree_nodes 1 in
       let expected_result = [Node ('x', Empty, Empty)] in
-      check (list (tree_pp Format.pp_print_char)) "Height 1" expected_result trees
+      check (list (of_pp (tree_pp Format.pp_print_char))) "Height 1" expected_result trees
     in
 
     let test_height_2 () =
@@ -35,13 +35,13 @@ module Make(Tested: Testable) : sig val run : unit -> unit end = struct
         Node ('x', Node ('x', Empty, Empty), Empty);
         Node ('x', Empty, Node ('x', Empty, Empty))
       ] in
-      check (list (tree_pp Format.pp_print_char)) "Height 2" expected_result trees
+      check (list (of_pp (tree_pp Format.pp_print_char))) "Height 2" expected_result trees
     in
 
     let test_height_0 () =
       let trees = hbal_tree_nodes 0 in
       let expected_result = [Empty] in
-      check (list (tree_pp Format.pp_print_char)) "Height 0" expected_result trees
+      check (list (of_pp (tree_pp Format.pp_print_char))) "Height 0" expected_result trees
     in
 
     [
